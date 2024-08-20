b45cc9923e177d4034d1bb9ed1fc1461665e1b20
Abdoulaye GAYE [SNT DSI/DAC/DIM/DS] <tmp_gaye74708@orange-sonatel.com>
2024-08-20 23:58:56 +0000

    update SecurityConfig.java file

--- src/main/java/app/auth/auth_jwt/config/SecurityConfig.java
+++ src/main/java/app/auth/auth_jwt/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
              .requestMatchers(authorizedUrl)
              .permitAll()
              .anyRequest()
-             .permitAll()
+             .authenticated()
              .and()
              .sessionManagement()
              .sessionCreationPolicy( SessionCreationPolicy.STATELESS)
