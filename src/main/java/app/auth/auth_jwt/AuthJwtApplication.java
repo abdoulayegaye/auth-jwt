e3f14e10b794ea3791c4de8f363a8a35c5c98775
Abdoulaye GAYE [SNT DSI/DAC/DIM/DS] <tmp_gaye74708@orange-sonatel.com>
2024-08-20 11:58:58 +0000

    First commit

---
+++ src/main/java/app/auth/auth_jwt/AuthJwtApplication.java
@@ -0,0 +1,13 @@
+package app.auth.auth_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/auth/auth_jwt/AuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package app.auth.auth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
